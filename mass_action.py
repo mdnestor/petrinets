e391cb49476dc3d843fd348aa85a32a88801eed6
mdnestor <mdnest01@gmail.com>
2023-02-16 21:52:57 -0500

    Update and rename time_dot.py to mass_action.py

--- mass_action.py
+++ mass_action.py
@@ -1,5 +1,6 @@
-import numpy as np
 from typing import List
+from petrinet import PetriNet
+import numpy as np
 
 def time_dot(net: PetriNet, x: List[float], rates: List[float]):
     S = net.species
@@ -8,10 +9,14 @@ def time_dot(net: PetriNet, x: List[float], rates: List[float]):
     E_out = net.edges_out
     k = rates
 
-    terms = np.array([k[t0] * np.prod([x[s] for (s,t) in E_in if t == t0]) for t0 in T])
+    S_idx = {s: i for (i, s) in enumerate(S)}
+    T_idx = {t: i for (i, t) in enumerate(T)}
+    
+    terms = np.array([k[T_idx[t0]] * np.prod([x[(S_idx[s])] for (s,t) in E_in if t == t0]) for t0 in T])
 
     count_in_list = lambda S, x: len([y for y in S if x == y])
     count = lambda s, t: count_in_list(E_out, (t, s)) - count_in_list(E_in, (s, t))
 
     D = np.array([[count(s, t) for s in S] for t in T])
-    return (np.transpose(D) @ terms).tolist()
+    x_dot = np.transpose(D) @ terms
+    return x_dot.tolist()
